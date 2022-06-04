fc58580e359f1549d3d8752d0fc64e0103422bd2
furchiks <104685157+furchiks@users.noreply.github.com>
2022-06-04 17:24:03 +0300

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -39,14 +39,30 @@ background = transform.scale(
 fps = 60
 clock = time.Clock()
 game = True
-player_1 = LeftPlayer('pingpong_rocket.png', 40, 80, 40, 210, 10)
-player_1.image = transform.rotate(player_1.image, 90)
+
+player_1 = LeftPlayer('pingpong_rocket.png', 10, 65, 40, 215, 10)
+player_2 = RightPlayer('pingpong_rocket.png', 10, 65, 750, 215, 10)
+ball = GamePlayer('pingpong_ball.png', 20, 20, 400, 250, 20)
+dx = 3
+dy = 3
+
+player_2.image = transform.rotate(player_2.image, 180)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if ball.rect.x >= 780 or ball.rect.x <= 0:
+        dx *= -1
+    if ball.rect.y >= 480 or ball.rect.y <= 0:
+        dy *= -1
+    ball.rect.x += dx
+    ball.rect.y += dy
     window.blit(background, (0, 0))
     player_1.update()
     player_1.reset()
+    player_2.update()
+    player_2.reset()
+    ball.reset()
     display.update()
     clock.tick(fps)
